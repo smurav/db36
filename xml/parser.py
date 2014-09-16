fab5ea2ed4ba8570cdf96014cfb20fbdd3cc91a6
Sergred <sergred@gmail.com>
2014-09-16 16:19:59 +0400

    Added validating function

--- xml/parser.py
+++ xml/parser.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
-import sys, libxml2
+#-*- coding: UTF-8 -*-
+import sys, libxml2, optparse
 
 def space(dep):
 	for i in range(0, dep):
@@ -33,10 +34,24 @@ def open(xml_file):
 	view(root)
  	doc.freeDoc()
 
+def validate(xml_file, dtd_file):
+	doc = libxml2.parseFile(xml_file)
+	dtd = libxml2.parseDTD(None, dtd_file)
+	ctxt = libxml2.newValidCtxt()
+	ret = doc.validateDtd(ctxt, dtd)
+	dtd.freeDtd()
+	doc.freeDoc()
+	return ret
+
 def main(argv):
-	if len(argv) != 2:
-		sys.stderr.write("Usage : %s xml_file" % (argv[0],))
-	else: open(argv[1])
+	op = optparse.OptionParser(description = U"Проверка на соответствие DTD", prog="dtd", version="0.1", usage=U"%prog")
+	op.add_option("-x", "--xml", dest="xml", help=U"XML документ", metavar="XML_FILE")
+	op.add_option("-d", "--dtd", dest="dtd", help=U"DTD документ", metavar="DTD_FILE")
+	options, arguments = op.parse_args()
+	if options.xml and options.dtd:
+		open(options.xml)
+		validate(options.xml, options.dtd)
+	else: op.print_help()
 
 if __name__ == '__main__':
 	main(sys.argv)
