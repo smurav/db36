cd346d464627398b2312759bf03733332078c62c
azamanov <zaman3915@mail.ru>
2014-09-29 00:55:27 +0400

    XML

---
+++ print.py
@@ -0,0 +1,14 @@
+mport sys, libxml2
+def open(xml_file): 
+ doc = libxml2.parseFile(xml_file)  
+ doc.freeDoc()
+
+def main(argv):
+ open(argv[1])
+from xml.dom.minidom import *
+
+xml = parse('mephi.xml')
+name = xml.getElementsByTagName('student')
+
+for node in name:
+	print node.childNodes[0].nodeValue
