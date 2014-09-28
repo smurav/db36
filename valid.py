78542e19eea865da2a862145dad9e89170d0222d
azamanov <zaman3915@mail.ru>
2014-09-29 00:45:01 +0400

    Revert "DTD"
    This reverts commit e8580ea4a87cb5455e391e5afa156dde9a7b66eb.

--- valid.py
+++ valid.py
@@ -12,7 +12,6 @@ def validate(xml_file, dtd_file):
   doc.freeDoc()
   return ret
 
-
 def main():
   op = optparse.OptionParser(description = U"Проверка на соответствие DTD", prog="dtd", version="0.1", usage=U"%prog")
   op.add_option("-x", "--xml", dest="xml", help=U"XML документ", metavar="XML_FILE")
