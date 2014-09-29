ec6fe03c055cfa5f280cd5930190c7cb1c926b0f
KiBmephi <virmant@mail.ru>
2014-09-29 20:03:27 +0400

    fix + add russian alternative

--- valid.py
+++ valid.py
@@ -3,28 +3,26 @@
 
 import optparse, libxml2, sys
 
-def validate(xml_file, dtd_file):
-  doc = libxml2.parseFile(xml_file)
-  dtd = libxml2.parseDTD(None, dtd_file)
-  ctxt = libxml2.newValidCtxt()
-  ret = doc.validateDtd(ctxt, dtd)
-  dtd.freeDtd()
-  doc.freeDoc()
-  return ret
+def schema_validate(xml_file, xsd_file):
+  ctxt = libxml2.schemaNewParserCtxt(xsd_file)
+  schema = ctxt.schemaParse()
+  validationCtxt = schema.schemaNewValidCtxt()
+  res = validationCtxt.schemaValidateFile(xml_file, 0)
+  if res != 0:
+    print "VALIDATION FAILED"
+  else:
+    print "VALIDATED"
+  return res
 
-raw_input
-def main():
-  op = optparse.OptionParser(description = U"ѕроверка на соответствие DTD", prog="dtd", version="0.1", usage=U"%prog")
+def main(argv):
+  op = optparse.OptionParser(description = U"ѕроверка на соответствие XSD", prog="xsd", version="0.1", usage=U"%prog")
   op.add_option("-x", "--xml", dest="xml", help=U"XML документ", metavar="XML_FILE")
-  op.add_option("-d", "--dtd", dest="dtd", help=U"DTD документ", metavar="DTD_FILE")
+  op.add_option("-s", "--xsd", dest="dtd", help=U"XSD документ", metavar="XSD_FILE")
 
   options, arguments = op.parse_args()
   if options.xml and options.dtd:
-    if validate(options.xml, options.dtd):
-      print "Successful!"
-    else:
-      print "Not Successful!"
+    validate(options.xml, options.xsd):
   else:
     op.print_help()
 if __name__=='__main__':
-  main()
+  main(sys.argv)
