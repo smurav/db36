60d0e7601c7b5aab4360dd49df2da3eafcb90767
asamsonov <samsonov68rus@gmail.com>
2014-10-21 00:05:41 +0400

    xslt added
    mephi.xsl - список кафедр
    2.xsl - добавление полного имени группы
    3.xsl - таблица всех студентов

--- xPath.py
+++ xPath.py
@@ -10,8 +10,17 @@ def printXP(x):
 	for i in x:
 		print i
 
-def maxNumOfStFac(doc, i, n):
-	pass
+def maxNumOfStFac(doc):
+	cnt = int(doc.xpath("count(//faculty)"))
+	print cnt
+	l = list()
+	k = list()
+	for i in xrange(1, cnt+1):
+		l.append(doc.xpath("count(//faculty["+str(i)+"]//child::student)"))
+		k.append(doc.xpath("string(//faculty["+str(i)+"]/attribute::name)"))
+	print "Faculty has max count of students is: " + k[l.index(max(l))]
+	print "Faculty has min count of students is: " + k[l.index(min(l))]
+
 
 def main(argv): 
 	doc = op(argv[1])
@@ -23,6 +32,7 @@ def main(argv):
 		else:
 			print doc.xpath(xp.pop(0).decode("utf-8"))
 		print 
+	maxNumOfStFac(doc)
 
 if __name__ == '__main__':
 	main(sys.argv)
