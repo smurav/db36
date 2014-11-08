dae757c9e01429470a624d2e4d33f9c018e093ec
Golovko Irina <irina.golovko.mephi@mail.ru>
2014-11-08 16:04:00 +0400

    xml file and parser

---
+++ listing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+import sys, libxml2
+
+def list(cell):
+	print
+	child = cell.children
+	while child.next is not None:
+		if child.type == "element":
+			list(child)
+		child = child.next
+	sys.stdout.write(child.content)
+
+def open(xml_file):
+	doc = libxml2.parseFile(xml_file)
+	root = doc.getRootElement()
+	list(root)
+	doc.freeDoc()
+
+def main(argv):
+	if len(argv) !=2:
+		sys.stdout.write("Usage: %s xml_file" % (argv[0],))
+	else:
+		open(argv[1])
+
+if __name__ == '__main__':
+	main(sys.argv)
